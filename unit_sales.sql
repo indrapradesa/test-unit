8e40593e48eb881127cbc0dd85e7399949a766b9
IT Kiosnet <indra@gmail.com>
2023-04-14 12:15:16 +0700

    db

--- unit_sales.sql
+++ unit_sales.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 11, 2023 at 11:34 AM
+-- Generation Time: Apr 14, 2023 at 07:13 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -71,10 +71,10 @@ INSERT INTO `cabang` (`idcabang`, `perusahaan_idperusahaan`, `nama`) VALUES
 CREATE TABLE `cust_closing` (
   `idcust_closing` int(10) UNSIGNED NOT NULL,
   `prospek_id` int(10) UNSIGNED NOT NULL,
-  `vprogram_id` int(10) UNSIGNED NOT NULL,
-  `provinsi_id` int(10) UNSIGNED NOT NULL,
+  `vprogram_id` int(10) UNSIGNED DEFAULT NULL,
+  `provinsi_id` int(10) UNSIGNED DEFAULT NULL,
   `jenis_cust_id` int(10) UNSIGNED NOT NULL,
-  `odp_id` int(10) UNSIGNED NOT NULL,
+  `odp_id` int(10) UNSIGNED DEFAULT NULL,
   `nik` int(10) UNSIGNED DEFAULT NULL,
   `nama` varchar(100) DEFAULT NULL,
   `almt_ktp` varchar(255) DEFAULT NULL,
@@ -85,28 +85,47 @@ CREATE TABLE `cust_closing` (
   `fto_ktp` varchar(255) DEFAULT NULL,
   `fto_rumah` varchar(255) DEFAULT NULL,
   `fto_sc_jalur` varchar(255) DEFAULT NULL,
-  `estimasi_kbl` int(50) UNSIGNED DEFAULT NULL
+  `estimasi_kbl` int(50) UNSIGNED DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `cust_closing`
 --
 
-INSERT INTO `cust_closing` (`idcust_closing`, `prospek_id`, `vprogram_id`, `provinsi_id`, `jenis_cust_id`, `odp_id`, `nik`, `nama`, `almt_ktp`, `tgl_lahir`, `jk`, `alamat_terpasang`, `koordinat`, `fto_ktp`, `fto_rumah`, `fto_sc_jalur`, `estimasi_kbl`) VALUES
-(1, 1, 1, 1, 1, 1, 1234567890, 'Ahmad', 'Jl. Pahlawan No. 1', '1990-01-01', 'L', 'Jl. Gatot Subroto No. 2', 1.2345, 'foto_ktp1.jpg', 'foto_rumah1.jpg', 'foto_sc_jalur1.jpg', 12),
-(2, 2, 2, 2, 2, 2, 2345678901, 'Budi', 'Jl. Ahmad Yani No. 2', '1991-02-02', 'P', 'Jl. Sudirman No. 3', 2.3456, 'foto_ktp2.jpg', 'foto_rumah2.jpg', 'foto_sc_jalur2.jpg', 23),
-(3, 3, 1, 3, 3, 3, 3456789012, 'Cici', 'Jl. Raya Bogor No. 3', '1992-03-03', 'L', 'Jl. Thamrin No. 4', 3.4567, 'foto_ktp3.jpg', 'foto_rumah3.jpg', 'foto_sc_jalur3.jpg', 34),
-(4, 4, 2, 4, 4, 4, 4294967295, 'Dodi', 'Jl. Raya Bekasi No. 4', '1993-04-04', 'P', 'Jl. Diponegoro No. 5', 4.5678, 'foto_ktp4.jpg', 'foto_rumah4.jpg', 'foto_sc_jalur4.jpg', 45),
-(5, 5, 1, 5, 5, 5, 4294967295, 'Eva', 'Jl. Raya Serpong No. 5', '1994-05-05', 'L', 'Jl. Imam Bonjol No. 6', 5.6789, 'foto_ktp5.jpg', 'foto_rumah5.jpg', 'foto_sc_jalur5.jpg', 56),
-(6, 6, 3, 6, 6, 6, 4294967295, 'Fani', 'Jl. Raya Tangerang No. 6', '1995-06-06', 'P', 'Jl. Jendral Sudirman No. 7', 6.789, 'foto_ktp6.jpg', 'foto_rumah6.jpg', 'foto_sc_jalur6.jpg', 67),
-(7, 1, 1, 1, 1, 1, 1234567890, 'John Doe', 'Jl. Sudirman No. 1', '1990-01-01', 'L', 'Jl. Kebon Jeruk No. 2', 123.456, 'ktp.jpg', 'rumah.jpg', 'jalur.jpg', 30),
-(8, 2, 1, 1, 1, 2, 2345678901, 'Jane Doe', 'Jl. Thamrin No. 2', '1992-02-02', 'P', 'Jl. Tanah Abang No. 3', 234.567, 'ktp.jpg', 'rumah.jpg', 'jalur.jpg', 45),
-(9, 3, 2, 2, 2, 3, 3456789012, 'Bob Smith', 'Jl. Sudirman No. 3', '1995-03-03', 'L', 'Jl. Kebayoran No. 4', 345.678, 'ktp.jpg', 'rumah.jpg', 'jalur.jpg', 60),
-(10, 4, 2, 2, 2, 4, 4294967295, 'Alice Smith', 'Jl. Thamrin No. 4', '1998-04-04', 'P', 'Jl. Tanah Abang No. 5', 456.789, 'ktp.jpg', 'rumah.jpg', 'jalur.jpg', 75),
-(11, 5, 1, 3, 3, 5, 4294967295, 'Peter Parker', 'Jl. Sudirman No. 5', '2001-05-05', 'L', 'Jl. Kebon Jeruk No. 6', 567.89, 'ktp.jpg', 'rumah.jpg', 'jalur.jpg', 90),
-(12, 6, 2, 3, 3, 6, 4294967295, 'Mary Jane', 'Jl. Thamrin No. 6', '2004-06-06', 'P', 'Jl. Tanah Abang No. 7', 678.901, 'ktp.jpg', 'rumah.jpg', 'jalur.jpg', 105),
-(13, 7, 2, 4, 4, 7, 4294967295, 'Tony Stark', 'Jl. Sudirman No. 7', '2007-07-07', 'L', 'Jl. Kebayoran No. 8', 789.012, 'ktp.jpg', 'rumah.jpg', 'jalur.jpg', 120),
-(14, 8, 1, 4, 4, 8, 4294967295, 'Pepper Potts', 'Jl. Thamrin No. 8', '2010-08-08', 'P', 'Jl. Tanah Abang No. 9', 890.123, 'ktp.jpg', 'rumah.jpg', 'jalur.jpg', 120);
+INSERT INTO `cust_closing` (`idcust_closing`, `prospek_id`, `vprogram_id`, `provinsi_id`, `jenis_cust_id`, `odp_id`, `nik`, `nama`, `almt_ktp`, `tgl_lahir`, `jk`, `alamat_terpasang`, `koordinat`, `fto_ktp`, `fto_rumah`, `fto_sc_jalur`, `estimasi_kbl`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, 1, 1, 1, 1234567890, 'Ahmad', 'Jl. Pahlawan No. 1', '1990-01-01', 'L', 'Jl. Gatot Subroto No. 2', 1.2345, 'foto_ktp1.jpg', 'foto_rumah1.jpg', 'foto_sc_jalur1.jpg', 12, '2023-04-12 18:04:37', '2023-04-12 18:04:37'),
+(2, 2, 2, 2, 2, 2, 2345678901, 'Budi', 'Jl. Ahmad Yani No. 2', '1991-02-02', 'P', 'Jl. Sudirman No. 3', 2.3456, 'foto_ktp2.jpg', 'foto_rumah2.jpg', 'foto_sc_jalur2.jpg', 23, '2023-04-12 18:04:37', '2023-04-12 18:04:37'),
+(3, 3, 1, 3, 3, 3, 3456789012, 'Cici', 'Jl. Raya Bogor No. 3', '1992-03-03', 'L', 'Jl. Thamrin No. 4', 3.4567, 'foto_ktp3.jpg', 'foto_rumah3.jpg', 'foto_sc_jalur3.jpg', 34, '2023-04-12 18:04:37', '2023-04-12 18:04:37'),
+(4, 4, 2, 4, 4, 4, 4294967295, 'Dodi', 'Jl. Raya Bekasi No. 4', '1993-04-04', 'P', 'Jl. Diponegoro No. 5', 4.5678, 'foto_ktp4.jpg', 'foto_rumah4.jpg', 'foto_sc_jalur4.jpg', 45, '2023-04-12 18:04:37', '2023-04-12 18:04:37'),
+(5, 5, 1, 5, 5, 5, 4294967295, 'Eva', 'Jl. Raya Serpong No. 5', '1994-05-05', 'L', 'Jl. Imam Bonjol No. 6', 5.6789, 'foto_ktp5.jpg', 'foto_rumah5.jpg', 'foto_sc_jalur5.jpg', 56, '2023-04-12 18:04:37', '2023-04-12 18:04:37'),
+(6, 6, 3, 6, 6, 6, 4294967295, 'Fani', 'Jl. Raya Tangerang No. 6', '1995-06-06', 'P', 'Jl. Jendral Sudirman No. 7', 6.789, 'foto_ktp6.jpg', 'foto_rumah6.jpg', 'foto_sc_jalur6.jpg', 67, '2023-04-12 18:04:37', '2023-04-12 18:04:37'),
+(7, 8, 1, 1, 1, 1, 1234567890, 'John Doe', 'Jl. Sudirman No. 1', '1990-01-01', 'L', 'Jl. Kebon Jeruk No. 2', 123.456, 'ktp.jpg', 'rumah.jpg', 'jalur.jpg', 30, '2023-04-12 18:04:37', '2023-04-12 18:04:37'),
+(8, 9, 1, 1, 1, 2, 2345678901, 'Jane Doe', 'Jl. Thamrin No. 2', '1992-02-02', 'P', 'Jl. Tanah Abang No. 3', 234.567, 'ktp.jpg', 'rumah.jpg', 'jalur.jpg', 45, '2023-04-12 18:04:37', '2023-04-12 18:04:37'),
+(9, 10, 2, 2, 2, 3, 3456789012, 'Bob Smith', 'Jl. Sudirman No. 3', '1995-03-03', 'L', 'Jl. Kebayoran No. 4', 345.678, 'ktp.jpg', 'rumah.jpg', 'jalur.jpg', 60, '2023-04-12 18:04:37', '2023-04-12 18:04:37'),
+(10, 11, 2, 2, 2, 4, 4294967295, 'Alice Smith', 'Jl. Thamrin No. 4', '1998-04-04', 'P', 'Jl. Tanah Abang No. 5', 456.789, 'ktp.jpg', 'rumah.jpg', 'jalur.jpg', 75, '2023-04-12 18:04:37', '2023-04-12 18:04:37'),
+(11, 12, 1, 3, 3, 5, 4294967295, 'Peter Parker', 'Jl. Sudirman No. 5', '2001-05-05', 'L', 'Jl. Kebon Jeruk No. 6', 567.89, 'ktp.jpg', 'rumah.jpg', 'jalur.jpg', 90, '2023-04-12 18:04:37', '2023-04-12 18:04:37'),
+(12, 13, 2, 3, 3, 6, 4294967295, 'Mary Jane', 'Jl. Thamrin No. 6', '2004-06-06', 'P', 'Jl. Tanah Abang No. 7', 678.901, 'ktp.jpg', 'rumah.jpg', 'jalur.jpg', 105, '2023-04-12 18:04:37', '2023-04-12 18:04:37'),
+(13, 14, 2, 4, 4, 7, 4294967295, 'Tony Stark', 'Jl. Sudirman No. 7', '2007-07-07', 'L', 'Jl. Kebayoran No. 8', 789.012, 'ktp.jpg', 'rumah.jpg', 'jalur.jpg', 120, '2023-04-12 18:04:37', '2023-04-12 18:04:37'),
+(14, 17, 1, 4, 4, 8, 4294967295, 'Pepper Potts', 'Jl. Thamrin No. 8', '2010-08-08', 'P', 'Jl. Tanah Abang No. 9', 890.123, 'ktp.jpg', 'rumah.jpg', 'jalur.jpg', 120, '2023-04-12 18:04:37', '2023-04-12 18:04:37'),
+(15, 21, NULL, NULL, 1, NULL, 1234546456, 'Indra Pradesa', 'asdafdfsdfwef', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2023-04-12 11:05:04', '2023-04-12 11:05:04'),
+(16, 22, NULL, NULL, 1, NULL, 5745654, 'sdfsf', 'sgerg', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2023-04-13 05:29:23', '2023-04-13 05:29:23'),
+(17, 24, NULL, NULL, 2, NULL, 54765478, 'adsasd', 'sefef', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2023-04-13 05:30:48', '2023-04-13 05:30:48');
+
+--
+-- Triggers `cust_closing`
+--
+DELIMITER $$
+CREATE TRIGGER `tr_cust_prospek_update_status` AFTER INSERT ON `cust_closing` FOR EACH ROW BEGIN
+  IF EXISTS (SELECT 1 FROM cust_closing WHERE prospek_id = NEW.prospek_id) THEN
+    UPDATE cust_prospek
+    SET status = 'Closing'
+    WHERE idcust_prospek = NEW.prospek_id;
+  END IF;
+END
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -121,7 +140,8 @@ CREATE TABLE `cust_prospek` (
   `sales_id` int(10) UNSIGNED NOT NULL,
   `nama` varchar(100) DEFAULT NULL,
   `alamat` varchar(255) DEFAULT NULL,
-  `no_tlpn` int(50) UNSIGNED DEFAULT NULL,
+  `no_tlpn` varchar(50) DEFAULT NULL,
+  `status` enum('Prospek','Closing') NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -130,34 +150,53 @@ CREATE TABLE `cust_prospek` (
 -- Dumping data for table `cust_prospek`
 --
 
-INSERT INTO `cust_prospek` (`idcust_prospek`, `status_id`, `metode_id`, `sales_id`, `nama`, `alamat`, `no_tlpn`, `created_at`, `updated_at`) VALUES
-(1, 1, 1, 1, 'Andi', 'Jl. Sudirman No. 10', 123456789, NULL, NULL),
-(2, 2, 2, 2, 'Budi', 'Jl. Thamrin No. 20', 987654321, NULL, NULL),
-(3, 1, 1, 3, 'Cindy', 'Jl. Gajah Mada No. 30', 2468101214, NULL, NULL),
-(4, 3, 3, 4, 'Dina', 'Jl. Diponegoro No. 40', 369121518, NULL, NULL),
-(5, 2, 1, 5, 'Eka', 'Jl. Kebon Jeruk No. 50', 1516171819, NULL, NULL),
-(6, 1, 2, 6, 'Fani', 'Jl. Gatot Subroto No. 60', 2021222324, NULL, NULL),
-(7, 3, 3, 7, 'Gina', 'Jl. Tanah Abang No. 70', 2526272829, NULL, NULL),
-(8, 2, 2, 8, 'Hadi', 'Jl. Cikini No. 80', 3031323334, NULL, NULL),
-(9, 1, 1, 1, 'Ika', 'Jl. Mangga Dua No. 90', 3536373839, NULL, NULL),
-(10, 3, 3, 2, 'Joni', 'Jl. Pemuda No. 100', 4041424344, NULL, NULL),
-(11, 2, 2, 3, 'Kiki', 'Jl. Pahlawan No. 110', 4294967295, NULL, NULL),
-(12, 1, 1, 4, 'Lia', 'Jl. Ahmad Yani No. 120', 4294967295, NULL, NULL),
-(13, 3, 3, 5, 'Mira', 'Jl. Raya Bogor No. 130', 4294967295, NULL, NULL),
-(14, 2, 2, 6, 'Nina', 'Jl. Raya Bekasi No. 140', 4294967295, NULL, NULL),
-(15, 1, 1, 7, 'Oki', 'Jl. Raya Serpong No. 150', 4294967295, NULL, NULL),
-(16, 3, 3, 8, 'Pia', 'Jl. Raya Tangerang No. 160', 4294967295, NULL, NULL),
-(17, 2, 2, 1, 'Qin', 'Jl. Raya Cirebon No. 170', 4294967295, NULL, NULL),
-(18, 1, 1, 2, 'Rika', 'Jl. Raya Solo No. 180', 4294967295, NULL, NULL),
-(19, 3, 3, 3, 'Sari', 'Jl. Raya Malang No. 190', 4294967295, NULL, NULL),
-(20, 2, 2, 4, 'Tina', 'Jl. Raya Bandung No. 200', 4294967295, NULL, NULL),
-(21, 1, 1, 1, 'Indra Pradesa', 'sfsd', 123, '2023-04-11 01:11:40', '2023-04-11 01:11:40'),
-(22, 1, 1, 1, 'sdf', 'sfsd', 2124, '2023-04-11 01:27:03', '2023-04-11 01:27:03'),
-(23, 1, 1, 1, 'sdf', 'sfsd', 123, '2023-04-11 01:30:47', '2023-04-11 01:30:47'),
-(24, 1, 1, 1, 'adsasd', 'asd', 123, '2023-04-11 01:33:10', '2023-04-11 01:33:10'),
-(25, 2, 2, 1, 'Adsasd', 'qwe', 123, '2023-04-11 01:34:29', '2023-04-11 01:34:29'),
-(26, 1, 1, 1, 'dfsd', 'asd', 123, '2023-04-11 01:43:35', '2023-04-11 01:43:35'),
-(27, 1, 1, 1, 'Adsasd', 'qwe', 123, '2023-04-11 02:07:23', '2023-04-11 02:07:23');
+INSERT INTO `cust_prospek` (`idcust_prospek`, `status_id`, `metode_id`, `sales_id`, `nama`, `alamat`, `no_tlpn`, `status`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, 1, 'Andi', 'Jl. Sudirman No. 10', '123456789', 'Closing', NULL, NULL),
+(2, 2, 2, 2, 'Budi', 'Jl. Thamrin No. 20', '987654321', 'Prospek', NULL, NULL),
+(3, 1, 1, 3, 'Cindy', 'Jl. Gajah Mada No. 30', '2468101214', 'Prospek', NULL, NULL),
+(4, 3, 3, 4, 'Dina', 'Jl. Diponegoro No. 40', '369121518', 'Prospek', NULL, NULL),
+(5, 2, 1, 5, 'Eka', 'Jl. Kebon Jeruk No. 50', '1516171819', 'Prospek', NULL, NULL),
+(6, 1, 2, 6, 'Fani', 'Jl. Gatot Subroto No. 60', '2021222324', 'Prospek', NULL, NULL),
+(7, 3, 3, 7, 'Gina', 'Jl. Tanah Abang No. 70', '2526272829', 'Prospek', NULL, NULL),
+(8, 2, 2, 8, 'Hadi', 'Jl. Cikini No. 80', '3031323334', 'Prospek', NULL, NULL),
+(9, 1, 1, 1, 'Ika', 'Jl. Mangga Dua No. 90', '3536373839', 'Closing', NULL, NULL),
+(10, 3, 3, 2, 'Joni', 'Jl. Pemuda No. 100', '4041424344', 'Prospek', NULL, NULL),
+(11, 2, 2, 3, 'Kiki', 'Jl. Pahlawan No. 110', '4294967295', 'Prospek', NULL, NULL),
+(12, 1, 1, 4, 'Lia', 'Jl. Ahmad Yani No. 120', '4294967295', 'Prospek', NULL, NULL),
+(13, 3, 3, 5, 'Mira', 'Jl. Raya Bogor No. 130', '4294967295', 'Prospek', NULL, NULL),
+(14, 2, 2, 6, 'Nina', 'Jl. Raya Bekasi No. 140', '4294967295', 'Prospek', NULL, NULL),
+(15, 1, 1, 7, 'Oki', 'Jl. Raya Serpong No. 150', '4294967295', 'Closing', NULL, NULL),
+(16, 3, 3, 8, 'Pia', 'Jl. Raya Tangerang No. 160', '4294967295', 'Prospek', NULL, NULL),
+(17, 2, 2, 1, 'Qin', 'Jl. Raya Cirebon No. 170', '4294967295', 'Closing', NULL, NULL),
+(18, 1, 1, 2, 'Rika', 'Jl. Raya Solo No. 180', '4294967295', 'Closing', NULL, NULL),
+(19, 3, 3, 3, 'Sari', 'Jl. Raya Malang No. 190', '4294967295', 'Prospek', NULL, NULL),
+(20, 2, 2, 4, 'Tina', 'Jl. Raya Bandung No. 200', '4294967295', 'Prospek', NULL, NULL),
+(21, 1, 1, 1, 'Indra Pradesa', 'sfsd', '123', 'Closing', '2023-04-11 01:11:40', '2023-04-11 01:11:40'),
+(22, 1, 1, 1, 'sdf', 'sfsd', '2124', 'Closing', '2023-04-11 01:27:03', '2023-04-11 01:27:03'),
+(23, 1, 1, 1, 'sdf', 'sfsd', '123', 'Prospek', '2023-04-11 01:30:47', '2023-04-11 01:30:47'),
+(24, 1, 1, 1, 'adsasd', 'asd', '123', 'Closing', '2023-04-11 01:33:10', '2023-04-11 01:33:10'),
+(25, 2, 2, 1, 'Adsasd', 'qwe', '123', 'Prospek', '2023-04-11 01:34:29', '2023-04-11 01:34:29'),
+(28, 1, 1, 1, 'adsasd', 'sfsd', '682223666840', 'Prospek', '2023-04-11 07:30:51', '2023-04-11 07:30:51'),
+(29, 1, 1, 1, 'adsasd', 'asd', '683214526845213', 'Prospek', '2023-04-11 07:31:39', '2023-04-11 07:31:39');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jenis_cust`
+--
+
+CREATE TABLE `jenis_cust` (
+  `idjenis_cust` int(10) UNSIGNED NOT NULL,
+  `kategori_cust` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `jenis_cust`
+--
+
+INSERT INTO `jenis_cust` (`idjenis_cust`, `kategori_cust`) VALUES
+(1, 'Perorangan'),
+(2, 'Business');
 
 -- --------------------------------------------------------
 
@@ -190,21 +229,22 @@ INSERT INTO `jenis_program` (`idjenis_program`, `nama`, `nominal`, `tanggal_dibu
 CREATE TABLE `karyawan` (
   `nip` int(10) UNSIGNED NOT NULL,
   `cabang_id` int(10) UNSIGNED NOT NULL,
-  `nama` varchar(100) DEFAULT NULL
+  `nama` varchar(100) DEFAULT NULL,
+  `email` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `karyawan`
 --
 
-INSERT INTO `karyawan` (`nip`, `cabang_id`, `nama`) VALUES
-(101, 1, 'Ali'),
-(102, 2, 'Budi'),
-(103, 1, 'Cici'),
-(104, 1, 'Dodi'),
-(105, 2, 'Eva'),
-(12345, 1, 'Indra Pradesa'),
-(123456, 2, 'Yanto Basna');
+INSERT INTO `karyawan` (`nip`, `cabang_id`, `nama`, `email`) VALUES
+(101, 1, 'Ali', ''),
+(102, 2, 'Budi', ''),
+(103, 1, 'Cici', ''),
+(104, 1, 'Dodi', ''),
+(105, 2, 'Eva', ''),
+(12345, 1, 'Indra Pradesa', 'indra@example.com'),
+(123456, 2, 'Yanto Basna', '');
 
 -- --------------------------------------------------------
 
@@ -502,13 +542,20 @@ ALTER TABLE `cabang`
 -- Indexes for table `cust_closing`
 --
 ALTER TABLE `cust_closing`
-  ADD PRIMARY KEY (`idcust_closing`);
+  ADD PRIMARY KEY (`idcust_closing`),
+  ADD UNIQUE KEY `prospek_id` (`prospek_id`);
 
 --
 -- Indexes for table `cust_prospek`
 --
 ALTER TABLE `cust_prospek`
   ADD PRIMARY KEY (`idcust_prospek`);
+
+--
+-- Indexes for table `jenis_cust`
+--
+ALTER TABLE `jenis_cust`
+  ADD PRIMARY KEY (`idjenis_cust`);
 
 --
 -- Indexes for table `jenis_program`
@@ -620,13 +667,19 @@ ALTER TABLE `cabang`
 -- AUTO_INCREMENT for table `cust_closing`
 --
 ALTER TABLE `cust_closing`
-  MODIFY `idcust_closing` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `idcust_closing` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `cust_prospek`
 --
 ALTER TABLE `cust_prospek`
-  MODIFY `idcust_prospek` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `idcust_prospek` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- AUTO_INCREMENT for table `jenis_cust`
+--
+ALTER TABLE `jenis_cust`
+  MODIFY `idjenis_cust` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `jenis_program`
