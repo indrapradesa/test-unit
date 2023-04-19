ed57d86566d6da41ee900e64f19064b6f55c2463
ALIFIA PUTRI BUDIYANTI <asiyah_xav@student.ub.ac.id>
2023-04-20 05:19:57 +0700

    Update index.blade.php

--- resources/views/d_sales/index.blade.php
+++ resources/views/d_sales/index.blade.php
@@ -1,7 +1,160 @@
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 @extends('d_sales.layouts.main')
 
 @section('container')
 
-    <h2 class="mb-4 text-3xl font-extrabold leading-none tracking-tight text-black md:text-4xl dark:text-white">{{ auth()->user()->karyawan->nama }}</h2>
+
+    <div class="grid grid-cols-2 gap-4 mt-8 mb-5 ">
+        <div class="  grid grid-rows-2 ">
+            <div class=" grid grid-cols-2 gap-4">
+                <div class="bg-gray-50 dark:bg-gray-900 dark:text-white h-28 rounded text-center py-5">
+                    <div class="font-bold text-2xl mb-2">Komisi</div>
+                    <div class="">100.400.000</div>
+                </div>
+                <div class="bg-gray-50  h-28 dark:bg-gray-900  dark:text-white rounded text-center py-5">
+                    <div class="font-bold text-2xl mb-2">Customer Closing</div>
+                    <div class="">5.000</div>
+                </div>
+            </div>
+
+
+                    <div class="bg-white rounded">
+
+
+                    </div>
+
+
+
+
+
+
+
+
+        </div>
+        <div class="">
+            <h3 class="dark:text-white text-black   font-bold text-lg   dark:border-white border-black mb-5">History Closing</h3>
+            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-white  uppercase bg-gray-400 dark:bg-gray-900 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            Bulan
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Jumlah
+                        </th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            Apple MacBook Pro 17"
+                        </th>
+                        <td class="px-6 py-4">
+                            Silver
+                        </td>
+
+                    </tr>
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            Microsoft Surface Pro
+                        </th>
+                        <td class="px-6 py-4">
+                            White
+                        </td>
+
+                    </tr>
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            Magic Mouse 2
+                        </th>
+                        <td class="px-6 py-4">
+                            Black
+                        </td>
+
+                    </tr>
+                    <tr class="bg-white dark:bg-gray-800 dark:border-gray-700">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            Apple MacBook Pro 17"
+                        </th>
+                        <td class="px-6 py-4">
+                            Silver
+                        </td>
+
+                    </tr>
+
+                </tbody>
+            </table>
+
+        </div>
+
+    </div>
+    <div class=" dark:border-gray-500 rounded border-black ">
+        <h3 class="text-xl font-bold my-5 dark:text-white">List Customer Prospek</h3>
+
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-white uppercase bg-gray-400 dark:bg-gray-900">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        Tanggal
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Nama
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Nomor Telepon
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Status
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        Apple MacBook Pro 17"
+                    </th>
+                    <td class="px-6 py-4">
+                        Silver
+                    </td>
+                    <td class="px-6 py-4">
+                        Laptop
+                    </td>
+                    <td class="px-6 py-4">
+                        $2999
+                    </td>
+                </tr>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        Microsoft Surface Pro
+                    </th>
+                    <td class="px-6 py-4">
+                        White
+                    </td>
+                    <td class="px-6 py-4">
+                        Laptop PC
+                    </td>
+                    <td class="px-6 py-4">
+                        $1999
+                    </td>
+                </tr>
+                <tr class="bg-white dark:bg-gray-800">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        Magic Mouse 2
+                    </th>
+                    <td class="px-6 py-4">
+                        Black
+                    </td>
+                    <td class="px-6 py-4">
+                        Accessories
+                    </td>
+                    <td class="px-6 py-4">
+                        $99
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
 
 @endsection
